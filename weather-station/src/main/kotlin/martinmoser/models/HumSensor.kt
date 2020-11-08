584f644598578784eca61512536bc48544a29e50
Moser Martin <mosermartin09@gmail.com>
2020-11-08 11:01:21 +0100

    Delete Hum and TempSensor

--- weather-station/src/main/kotlin/martinmoser/models/HumSensor.kt
+++
@@ -1,7 +0,0 @@
-package martinmoser.models
-
-/*class HumSensor(
-        name: String,
-        value_type: martinmoser.models.ValueType,
-        unit: String
-) : Sensor(name, value_type, unit)*/
--- weather-station/src/main/kotlin/martinmoser/models/TempSensor.kt
+++
@@ -1,7 +0,0 @@
-package martinmoser.models
-
-/*class TempSensor(
-        name: String,
-        value_type: martinmoser.models.ValueType,
-        unit: String,
-) : Sensor(name, value_type, unit)*/
