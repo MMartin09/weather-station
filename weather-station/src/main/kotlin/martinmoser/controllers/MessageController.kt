e15e686cf55fc1f6b817da0a718908a3e628cd2f
Moser Martin <mosermartin09@gmail.com>
2020-11-08 14:29:21 +0100

    Create controllers package and move the MessageController into it

---
+++ weather-station/src/main/kotlin/martinmoser/controllers/MessageController.kt
@@ -0,0 +1,71 @@
+package martinmoser.controllers
+
+import javafx.beans.property.SimpleStringProperty
+import tornadofx.Controller
+import java.time.LocalTime
+
+
+/**
+ * Controller for the message window.
+ *
+ * @author MMartin09
+ * @since 0.1.0
+ */
+class MessageController: Controller() {
+    private val message = SimpleStringProperty("")
+    private val messageArray = mutableListOf<String>()
+
+    /**
+     * Add a new message.
+     *
+     * Add a message to the message box.
+     * Each new message is printed into a new line.
+     *
+     * @author MMartin09
+     * @since 0.1.0
+     *
+     * @param msg Message that should be printed
+     */
+    fun addMessage(msg: String) {
+        if (messageArray.size >= 3) deleteLine()
+
+        messageArray.prepend(LocalTime.now().toString() + ": " + msg + "\n")
+        val tmp = messageArray.toList().toString().replace("[", "").replace("]", "").replace(", ", "")
+
+        message.set(tmp)
+    }
+
+    /**
+     * Get all messages.
+     *
+     * @returns The messages for the message box.
+     */
+    fun getMessages(): SimpleStringProperty { return message }
+
+    /**
+     * Delete the oldest message.
+     *
+     * Remove the oldest message from the messages array.
+     * Since new messages are added to the front, the oldest message is the end of the list.
+     *
+     * @author MMartin09
+     * @since 0.1.0
+     */
+    private fun deleteLine() {
+        messageArray.removeLast()
+    }
+
+    /**
+     * Add new item at the front of the message list.
+     *
+     * Adds a new item at the front of the message list.
+     *
+     * @author MMartin09
+     * @since 0.1.0
+     *
+     * @param element Element that should be added
+     */
+    private fun MutableList<String>.prepend(element: String) {
+        add(0, element)
+    }
+}
