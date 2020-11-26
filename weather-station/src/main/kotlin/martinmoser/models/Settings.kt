f3022b4f5af36acca31168c03a436ca8cf35f54a
Moser Martin <mosermartin09@gmail.com>
2020-11-26 11:02:07 +0100

    Create SensorModel

--- weather-station/src/main/kotlin/martinmoser/models/Settings.kt
+++ weather-station/src/main/kotlin/martinmoser/models/Settings.kt
@@ -1,5 +1,7 @@
 package martinmoser.models
 
+import tornadofx.ItemViewModel
+
 /**
  * Settings class holds all settings for the application
  *
@@ -9,3 +11,15 @@ package martinmoser.models
 data class Settings(
     val decimal_places: Int
 )
+
+/**
+ * ItemViewModel for the Settings class.
+ *
+ * The model binds the required attributes from the settings.
+ *
+ * @author MMartin09
+ * @since 0.1.0
+ */
+class SettingsModel: ItemViewModel<Settings>() {
+    val decimal_places = bind(Settings::decimal_places)
+}
