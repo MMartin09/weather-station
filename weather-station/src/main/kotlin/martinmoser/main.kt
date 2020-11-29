2c4aedabb0c34e5c43de67b4692a2afcae4c3a22
Moser Martin <mosermartin09@gmail.com>
2020-11-29 13:43:14 +0100

    Update test code for database

--- weather-station/src/main/kotlin/martinmoser/main.kt
+++ weather-station/src/main/kotlin/martinmoser/main.kt
@@ -37,13 +37,6 @@ object Sensors : Table() {
     override val primaryKey = PrimaryKey(id, name = "PK_Sensors_ID")
 }
 
-object Cities : Table() {
-    val id = integer("id").autoIncrement() // Column<Int>
-    val name = varchar("name", 50) // Column<String>
-
-    override val primaryKey = PrimaryKey(id, name = "PK_Cities_ID")
-}
-
 /**
  * Main function.
  *
@@ -53,53 +46,25 @@ object Cities : Table() {
  * @since 0.1.0
  */
 fun main(args: Array<String>) {
-    //Database.connect("jdbc:sqlite:./data.db", "org.sqlite.JDBC")
-    //Database.connect("jdbc:sqlite:file:test?mode=memory&cache=shared", "org.sqlite.JDBC")
-
     Database.connect("jdbc:h2:mem:test", driver = "org.h2.Driver", user = "root", password = "")
 
     transaction {
         addLogger(StdOutSqlLogger)
 
-        SchemaUtils.create(Cities, Sensors)
+        SchemaUtils.create(Sensors)
 
         val id = Sensors.insert {
             it[name] = "Temp Sensor"
         } get Sensors.id
 
-        println("New sensor id: $id")
-
         val iid = Sensors.insert {
             it[name] = "Hum Sensor"
         } get Sensors.id
 
-        println("New sensor id: $iid")
-
-        val sensors = Sensors.selectAll()
-
-        val saintPetersburgId = Cities.insert {
-            it[name] = "St. Petersburg"
-        } get Cities.id
-
-        val munichId = Cities.insert {
-            it[name] = "Munich"
-        } get Cities.id
-
-        val pragueId = Cities.insert {
-            it.update(name, stringLiteral("   Prague   ").trim().substring(1, 2))
-        }[Cities.id]
-
-        val pragueName = Cities.select { Cities.id eq pragueId }.single()[Cities.name]
-
-        for (city in Cities.selectAll()) {
-            println("${city[Cities.id]}: ${city[Cities.name]}")
+        for (sensor in Sensors.selectAll()) {
+            println("${sensor[Sensors.id]}: ${sensor[Sensors.name]}")
         }
 
-        if (sensors != null) {
-            sensors.forEach {
-                println("${it}")
-            }
-        }
     }
 
     launch<MainApp>(args)
